12242664633f2bb971f685eb1edd1a20f6a5390b
Nicolas Sanhueza <n.sanhueza05@ufromail.cl>
2023-09-25 20:09:04 -0300

    Agregada funcionalidad para recomendar 2 juegos aleatorios por consola

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const app = express();
 
 setupRoutes(app);
 
+
+/**
+ * Starts the server and listens on port 3000.
+ * 
+ * @returns {void} - While the function does not return any value, it starts the server and listens on port 3000.
+ */
+
 const startServer = async () => {
-    app.listen(3000, () => {
+    await app.listen(3000, () => {
         console.log('Server is running on port 3000');
     })
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const { getRecommendationConsole, getRecommendationWithGenre } = require("../controllers/consoles.controller");
 const { CONSOLES_PREFIX } = require("../utils/constants");
 
+/**
+ * Sets up the routes for handling HTTP requests in the APP.
+ * 
+ * @param {Object} app - An instance of the Express.js application.
+ * @returns {void} - Once complete it returns nothing.
+ */
 const setupRoutes = (app) => {
-    app.use(CONSOLES_PREFIX+'/:console/random_games',getRecommendationConsole);
-    app.use(CONSOLES_PREFIX+'/:console/:genre/random_game',getRecommendationWithGenre);
+    app.use(CONSOLES_PREFIX + '/:console/random_games', getRecommendationConsole);
+    app.use(CONSOLES_PREFIX + '/:console/:genre/random_game', getRecommendationWithGenre);
 }
 
 module.exports = setupRoutes;

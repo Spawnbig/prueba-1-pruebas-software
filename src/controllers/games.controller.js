e7cdff0ac7c5486358fa2041cb52c83780fc4a37
NICOLAS IGNACIO RIVAS SAGREDO <n.rivas03>
2023-09-25 23:10:41 -0300

    Add documentation

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,18 +1,29 @@
 const { findVideoGameByName } = require("../services/games.service");
 
+/**
+ * Retrieves a video game by its name from the database.
+ * @async
+ * @function getGameByName
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Object} - The response object with the game data or an error message.
+ */
 const getGameByName = async (req, res) => {
     const gameName = req.query.name;
+    if (typeof gameName !== 'string' || gameName.trim() === '') {
+        res.status(400).json({ error: "Invalid game name" });
+        return;
+    }
     try {
         const game = await findVideoGameByName(gameName);
         if (game) {
-            const formattedResponse = `${game.video_console} - ${game.genres.join(", ")}`;
-            res.status(200).json({ response: formattedResponse });
+            res.status(200).json({ game });
         } else {
             res.status(404).json({ error: "El juego no existe" });
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Error al buscar el juego por nombre" });
+        res.status(500).json({ error: "Error al buscar el juego por nombre", details: error.message });
     }
 };
 
--- src/services/games.service.js
+++ src/services/games.service.js
@@ -1,12 +1,25 @@
 const videoGames = require("../utils/load-games");
 
 
+/**
+ * Finds a video game by its name.
+ * @async
+ * @function
+ * @param {string} gameName - The name of the video game to find.
+ * @returns {Object} - The video game object if found, otherwise undefined.
+ */
 const findVideoGameByName = async (gameName) => {
     const modifyName = deleteQuotationMarks(gameName);
     const gamesList = Object.values(videoGames).flatMap(gamesForConsole => gamesForConsole);
     return gamesList.find(game => game.name.toUpperCase() === modifyName.toUpperCase());
 }
 
+/**
+ * Removes all quotation marks from a given text.
+ *
+ * @param {string} text - The text to remove quotation marks from.
+ * @returns {string} The input text without any quotation marks.
+ */
 function deleteQuotationMarks(text) {
     const textWithOutQuotations = text.replace(/"/g, "");
     return textWithOutQuotations;

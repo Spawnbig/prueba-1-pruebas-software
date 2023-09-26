325e75ab9b1aa8fb72636b80aabceafc758272d6
NICOLAS IGNACIO RIVAS SAGREDO <n.rivas03>
2023-09-25 23:23:21 -0300

    Fix game response

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -10,20 +10,17 @@ const { findVideoGameByName } = require("../services/games.service");
  */
 const getGameByName = async (req, res) => {
     const gameName = req.query.name;
-    if (typeof gameName !== 'string' || gameName.trim() === '') {
-        res.status(400).json({ error: "Invalid game name" });
-        return;
-    }
     try {
         const game = await findVideoGameByName(gameName);
         if (game) {
-            res.status(200).json({ game });
+            const formattedResponse = `${game.video_console} - ${game.genres.join(", ")}`;
+            res.status(200).json({ response: formattedResponse });
         } else {
             res.status(404).json({ error: "El juego no existe" });
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Error al buscar el juego por nombre", details: error.message });
+        res.status(500).json({ error: "Error al buscar el juego por nombre" });
     }
 };
 
